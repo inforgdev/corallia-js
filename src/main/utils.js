6dc4c742abc817a9a942079e1f3df72ef4849160
Luis Float <contact@luisfloat.com>
2022-07-03 17:17:44 -0300

    fix prop name to filename

--- src/main/utils.js
+++ src/main/utils.js
@@ -3,7 +3,7 @@ import path from "path";
 function dist(outOptions) {
     let dirname = outOptions.dirname;
     let extname = outOptions.extname;
-    let name = outOptions.name;
+    let name = outOptions.filename;
     if (!extname.startsWith(".")) extname = `.${extname}`;
     return path.join(dirname, name).concat(`${extname}`);
 }
