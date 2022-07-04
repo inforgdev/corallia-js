a9994598a7042d032cc8450deb1a2f7a629a9d93
Luis Float <contact@luisfloat.com>
2022-07-03 22:42:21 -0300

    fix indentation lint error

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,16 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 
 export default {
-  input: "./src/main/index.js",
-  output: {
-    file: "./dist/corallia.js"
-  },
-  external: [
-    "fs",
-    "path",
-  ],
-  plugins: [
-    nodeResolve(),
-    terser()
-  ]
+    input: "./src/main/index.js",
+    output: {
+        file: "./dist/corallia.js"
+    },
+    external: [
+        "fs",
+        "path",
+    ],
+    plugins: [
+        nodeResolve(),
+        terser()
+    ]
 };
