fc06d6f9ced7c5b9fe1edd90188ac63bbf60b2cb
Luis Float <contact@luisfloat.com>
2022-08-19 19:16:34 -0300

    refactor(main): rem obsolete langs

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,9 @@
 import corallia from "./corallia";
-import langs from "./langs";
 import stringify from "./stringify";
 import stringifyFile from "./stringifyFile";
 
 export {
     corallia,
-    langs,
     stringify,
     stringifyFile,
 };
