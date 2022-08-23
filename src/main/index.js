95efc7e739c34aafc1e23852b2448d7448c5f39d
Luis Float <contact@luisfloat.com>
2022-08-23 17:54:49 -0300

    refactor(main): rem obsolete files

--- src/main/index.js
+++ src/main/index.js
@@ -1,11 +1,5 @@
 import coralliaProc from "./coralliaProc";
-import corallia from "./corallia";
-import stringify from "./stringify";
-import stringifyFile from "./stringifyFile";
 
 export {
     coralliaProc,
-    corallia,
-    stringify,
-    stringifyFile,
 };
--- src/main/langs/index.js
+++
@@ -1,9 +0,0 @@
-import scss from "./scss.js";
-import less from "./less.js";
-import styl from "./styl.js";
-
-export default {
-    scss,
-    less,
-    styl,
-};
