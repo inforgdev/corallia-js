55542dc3df61b9939fb0deafb610959335532a7f
Luis Float <contact@luisfloat.com>
2022-08-23 18:34:02 -0300

    bug(example): use `coralliaProc.js`

--- src/example/index.js
+++ src/example/index.js
@@ -1,23 +1,8 @@
 import { langs } from "octopo-js";
-import corallia from "../main/corallia.js";
+import coralliaProc from "../main/coralliaProc.js";
 
 import tokens from "./tokens/index.js";
 var lang = langs.scss;
 
-corallia({
-    in: {
-        data: tokens,
-    },
-    proc: {
-        minify: false,
-        grammar: lang,
-    },
-    out: {
-        file: {
-            dirname: "./src/example/dest/",
-            name: "example",
-            extname: lang.extname,
-        },
-        print: true,
-    },
-});
+const output = coralliaProc(tokens, { grammar: lang });
+console.log(output);
