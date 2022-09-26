9ca246c44a04faf1fc0714cc18f492a71f0d8665
Luis Float <contact@luisfloat.com>
2022-09-25 22:41:02 -0300

    feat(build): add cjs support

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,23 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
+import fs from "fs";
+
+const pkg = JSON.parse(fs.readFileSync("package.json"));
 
 export default {
     input: "./src/main/index.js",
-    output: {
-        file: "./dist/corallia.js",
-    },
+    output: [
+        {
+            file: pkg.module,
+            format: "esm",
+            plugins: [ terser() ],
+        },
+        {
+            file: pkg.main,
+            format: "cjs",
+            plugins: [ terser() ],
+        },
+    ],
     external: [
         "fs",
         "path",
