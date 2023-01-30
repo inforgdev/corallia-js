8acd7b8b583c011cb24b2da3fa6279e5839b15e2
Luis Float <contact@luisfloat.com>
2023-01-29 22:16:04 -0300

    refactor(main): remove example from the main branch

--- src/example/index.js
+++
@@ -1,8 +0,0 @@
-import { langs } from "octopo-js";
-import { octopizeTokens } from "../main/octopizeTokens.js";
-
-import * as tokens from "./tokens/index.js";
-var lang = langs.scss;
-
-const output = octopizeTokens(tokens, lang);
-console.log(output);
--- src/example/tokens/index.js
+++
@@ -1,7 +0,0 @@
-import color from "./color.js";
-import space from "./space.js";
-
-export {
-    color,
-    space
-};
